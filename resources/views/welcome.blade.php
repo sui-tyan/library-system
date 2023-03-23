dd12876ebb06aa105aad88b73bdff540f8c37352
Apple Juice <mallari23adrian@gmail.com>
2023-03-23 22:35:15 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css','resources/js/app.js'])
+        <!-- Styles -->
+    </head>
+    <body class="antialiased">
+        <x-nav />
+        <section class="bg-whitebg-no-repeat h-[75vh] bg-cover filter brightness-50" style="background-image: url({{url('/images/UPang.jpg')}})">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+                    <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
+                </div> 
+            </div>
+        </section>
+    </body>
+</html>
